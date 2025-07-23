1a87eae4690ecfabb63b5dd866e4fde77bfd364f
dharaneesh <rvi13970@gmail.com>
2025-07-23 12:41:28 +0530

    chore: initialize project structure and configuration

---
+++ trade-nest/src/main/java/com/dharaneesh/trade_nest/TradeNestApplication.java
@@ -0,0 +1,13 @@
+package com.dharaneesh.trade_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeNestApplication.class, args);
+	}
+
+}
---
+++ trade-nest/src/test/java/com/dharaneesh/trade_nest/TradeNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharaneesh.trade_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
