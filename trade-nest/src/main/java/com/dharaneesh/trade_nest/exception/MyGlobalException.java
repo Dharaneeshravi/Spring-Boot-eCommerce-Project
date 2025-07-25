9c02f6c6816869bd083591b3f54022a77eba44bd
dharaneesh <rvi13970@gmail.com>
2025-07-25 22:12:55 +0530

    feat: setup MyGlobalException class

---
+++ trade-nest/src/main/java/com/dharaneesh/trade_nest/exception/MyGlobalException.java
@@ -0,0 +1,28 @@
+package com.dharaneesh.trade_nest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalException {
+
+ @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e)
+     {
+         Map<String,String> response=new HashMap<>();
+         e.getBindingResult().getAllErrors().forEach(objectError ->{
+
+             String fieldName=((FieldError)objectError).getField();
+             String message=objectError.getDefaultMessage();
+             response.put(fieldName,message);
+         });
+         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+     }
+}
